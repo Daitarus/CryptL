2e85963fbc9afb78fa4d6fd359822ddf4f5a717c
PavelChaikovskii <chajkovskij_99@mail.ru>
2025-03-19 16:36:17 +0300

    add doc comments for ICrypt

--- Crypt/ICrypt.cs
+++ Crypt/ICrypt.cs
@@ -1,8 +1,22 @@
 ﻿namespace CryptL.Crypt
 {
+    /// <summary>
+    /// Interface for working with encryption.
+    /// </summary>
     public interface ICrypt
     {
-        public abstract byte[] Encrypt(byte[] originalData);
-        public abstract byte[] Decrypt(byte[] encryptData);        
+        /// <summary>
+        /// Encrypts data.
+        /// </summary>
+        /// <param name="originalData">Original data in the form of a byte array.</param>
+        /// <returns>Encrypted data in the form of a byte array.</returns>
+        public byte[] Encrypt(byte[] originalData);
+
+        /// <summary>
+        /// Decrypt data.
+        /// </summary>
+        /// <param name="encryptData">Encrypted data in the form of a byte array.</param>
+        /// <returns>Decrypted data in the form of a byte array.</returns>
+        public byte[] Decrypt(byte[] encryptData);        
     }
 }
