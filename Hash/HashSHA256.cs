4d3cdace1b83a189cdc61ba9d92e93f83895064a
PavelChaikovskii <chajkovskij_99@mail.ru>
2025-03-19 15:21:52 +0300

    add interface for Hash

--- Hash/HashSHA256.cs
+++ Hash/HashSHA256.cs
@@ -1,25 +1,28 @@
 ﻿using System.Security.Cryptography;
 using System.Text;
 
-namespace CryptL.Hash
-{
-    public static class HashSHA256
-    {
-        public static int Length { get { return 32; } }
-        public static byte[] GetHash(byte[] originalData)
-        {
-            if (originalData == null || originalData.Length == 0)
-                throw new ArgumentNullException(nameof(originalData));
+namespace CryptL.Hash;
 
-            using (SHA256 sha256 = SHA256.Create())
-            {
-                return sha256.ComputeHash(originalData);
-            }
-        }
-        public static byte[] GetHash(string originaData)
-        {
-            byte[] origonalDataBytes = Encoding.UTF8.GetBytes(originaData);
-            return GetHash(origonalDataBytes);
-        }
+/// <summary>
+/// Hashes data based on SHA256.
+/// </summary>
+public class HashSHA256 : IHash
+{
+    /// <inheritdoc/>
+    public byte[] GetHash(byte[] originalData)
+    {
+        if (originalData == null || originalData.Length == 0)
+            throw new ArgumentNullException(nameof(originalData));
+
+        using var sha256 = SHA256.Create();
+
+        return sha256.ComputeHash(originalData);
+    }
+
+    /// <inheritdoc/>
+    public byte[] GetHash(string originalData)
+    {
+        var origonalDataBytes = Encoding.UTF8.GetBytes(originalData);
+        return GetHash(origonalDataBytes);
     }
 }
---
+++ Hash/IHash.cs
@@ -0,0 +1,21 @@
+﻿namespace CryptL.Hash;
+
+/// <summary>
+/// Interface for working with hashing data.
+/// </summary>
+public interface IHash
+{
+    /// <summary>
+    /// Hashes the data.
+    /// </summary>
+    /// <param name="originalData">Hashed data in the form of an array of bytes.</param>
+    /// <returns>Hash of data in the form of an array of bytes.</returns>
+    byte[] GetHash(byte[] originalData);
+
+    /// <summary>
+    /// Hashes the data.
+    /// </summary>
+    /// <param name="originalData">Hashed data in the form of a string.</param>
+    /// <returns>Hash of data in the form of an array of bytes.</returns>
+    byte[] GetHash(string originalData);
+}
